080a384433ee51ec638ceb11c0c994a2e9910520
MiraGorte <sushchinskaya.m@gmail.com>
2023-04-28 14:24:59 +0300

    Initial commit

--- homework.py
+++ homework.py
@@ -8,7 +8,11 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
+    if 6 <= current_time.hour < 22:
+       is_dark_theme = False
+    else:
+       is_dark_theme = True
+
     assert is_dark_theme is True
 
 
@@ -25,7 +29,12 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
+    if dark_theme_enabled_by_user:
+        is_dark_theme = True
+    elif 6 <= current_time.hour < 22 or dark_theme_enabled_by_user is False:
+        is_dark_theme = False
+    else:
+        is_dark_theme = True
     assert is_dark_theme is True
 
 
